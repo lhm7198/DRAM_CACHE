433f2a2b59da1d5f3dd5d2f6d72deaeded3d5d7e
LeeJiHo <leej1593@gmail.com>
2022-09-04 16:19:21 +0900

    index_extarctor_tb

--- sim/tb/INDEX_EXTRACTOR_tb.sv
+++ sim/tb/INDEX_EXTRACTOR_tb.sv
@@ -24,6 +24,7 @@ wire [127 : 0]	fifo_i;
 localparam CLOCK_PERIOD = 1000;
 always #(CLOCK_PERIOD/2) clk = ~clk;
 
+int i;
 initial
 begin
 	rst_n = 1'b1;
@@ -47,18 +48,23 @@ begin
 
 	$display("\nStart");
 
-	#(CLOCK_PERIOD);
-	$display("ready_o = %d, slave_i = %d, fifo_write_enable = %d, fifo_i = %d\n", ready_o, slave_i, fifo_write_enable, fifo_i);	
+	for(i=0 ; i<5 ; i++) begin
+		#(CLOCK_PERIOD);
+		$display("ready_o = %d, slave_i = %d, fifo_write_enable = %d, fifo_i = %d\n", ready_o, slave_i, fifo_write_enable, fifo_i);	
 	
-	arid_i = 1;
-	arvalid_i = 1;
-	araddr_i = 100;
+		if($urandom % 2 == 0) begin
+			arid_i = i;
+			arvalid_i = 1;
+			araddr_i = $urandom % 256;
+		end else
+			awid_i = i;
+			awvalid_i = 1;
+			awaddr_i = $urandom % 256;
+		end
+		#(CLOCK_PERIOD);
 
-	$display("ready_o = %d, slave_i = %d, fifo_write_enable = %d, fifo_i = %d\n", ready_o, slave_i, fifo_write_enable, fifo_i);	
-	#(CLOCK_PERIOD);
-	
-
-	$display("ready_o = %d, slave_i = %d, fifo_write_enable = %d, fifo_i = %d\n", ready_o, slave_i, fifo_write_enable, fifo_i);	
+		$display("ready_o = %d, slave_i = %d, fifo_write_enable = %d, fifo_i = %d\n", ready_o, slave_i, fifo_write_enable, fifo_i);	
+	end
 	$finish;
 end
 

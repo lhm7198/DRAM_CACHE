ecdd50bdd305ee0651e2519c51285fba9750b431
leejh1593 <leejh1593@google.com>
2022-09-06 20:37:54 +0900

    index_extractor

--- rtl/index_extractor.sv
+++ rtl/index_extractor.sv
@@ -30,8 +30,7 @@ module INDEX_EXTRACTOR # (
 
 localparam 			S_IDLE	= 2'd0,
 				S_RREQ 	= 2'd1,
-				S_WREQ	= 2'd2,
-				S_FIFO	= 2'd3;
+				S_WREQ	= 2'd2;
 
 reg	[1:0]			state,		state_n;
 
@@ -43,7 +42,7 @@ reg				arbiter,	arbiter_n;
 reg				arready,
 				awready;
 
-always_ff @(posedge clk)
+always @(posedge clk) begin
 	if (!rst_n) begin
 		state		<= S_IDLE;
 		
@@ -51,68 +50,55 @@ always_ff @(posedge clk)
 		fifo_data	<= 128'd0;
 		fifo_write_en	<= 0;
 		arbiter		<= 0;
+
+		state_n		<= S_IDLE;
+		
+		index_n		<= 4'd0;
+		fifo_data_n	<= 128'd0;
+		fifo_write_en_n	<= 0;
+		arbiter_n	<= 0;
+
 	end 
 	else begin
+		case (state)
+			S_IDLE: begin
+				if(arvalid_i && (!awvalid_i || !arbiter)) begin
+					state_n					= S_RREQ;
+					arbiter_n 				= 1'b1;
+				end
+				else if(awvalid_i && (!arvalid_i || arbiter)) begin
+					state_n					= S_WREQ;
+					arbiter_n 				= 1'b0;
+				end
+			end
+			S_RREQ: begin
+				arready						= 1'b0;
+				index_n 					= araddr_i[INDEX_BIT_SIZE-1 : 0];
+				fifo_data_n[0:0]				= 1'b0; 				//read
+				fifo_data_n[ID_WIDTH:1]				= arid_i;
+				fifo_data_n[ADDR_WIDTH+ID_WIDTH:ID_WIDTH+1]	= araddr_i;
+				state_n						= S_IDLE;
+			end
+			S_WREQ: begin
+				awready						= 1'b0;
+				index_n 					= awaddr_i[INDEX_BIT_SIZE-1 : 0];
+				fifo_data_n[0:0]				= 1'b1; 				//write
+				fifo_data_n[ID_WIDTH:1]				= awid_i;
+				fifo_data_n[ADDR_WIDTH+ID_WIDTH:ID_WIDTH+1]	= awaddr_i;
+				state_n						= S_IDLE;
+			end
+		endcase
+
 		state		<= state_n;
 
 		index		<= index_n;
 		fifo_data	<= fifo_data_n;
 		fifo_write_en	<= fifo_write_en_n;
 		arbiter		<= arbiter_n;
+		
+		arready		= !fifo_afull_i;
+		awready		= !fifo_afull_i;
 	end
-
-always_comb
-begin
-	state_n		= state;
-
-	index_n		= index;
-	fifo_data_n	= fifo_data;
-	fifo_write_en_n	= fifo_write_en;
-	arbiter_n	= arbiter;
-
-	arready		= 1'b1;
-	awready		= 1'b1;
-
-	case (state)
-		S_IDLE: begin
-			if(arvalid_i && (!awvalid_i || !arbiter)) begin
-				state_n					= S_RREQ;
-				arbiter_n 				= 1'b1;
-			end
-			else if(awvalid_i && (!arvalid_i || arbiter)) begin
-				state_n					= S_WREQ;
-				arbiter_n 				= 1'b0;
-			end
-		end
-		S_RREQ: begin
-			arready						= 1'b0;
-
-			index_n 					= araddr_i[INDEX_BIT_SIZE-1 : 0];
-			fifo_data_n[0:0]				= 1'b0; 				//read
-			if(fifo_afull_i == 0) begin
-				state_n					= S_FIFO;
-			end
-		end
-		S_WREQ: begin
-			awready						= 1'b0;
-
-			index_n 					= araddr_i[INDEX_BIT_SIZE-1 : 0];
-			fifo_data_n[0:0]				= 1'b1; 				//write
-			if(fifo_afull_i == 0) begin
-				state_n					= S_FIFO;
-			end
-		end
-		S_FIFO: begin
-			arready						= 1'b0;
-			awready						= 1'b0;
-
-			fifo_data_n[ID_WIDTH:1]				= arid_i;
-			fifo_data_n[ADDR_WIDTH+ID_WIDTH : ID_WIDTH+1]	= araddr_i[ADDR_WIDTH-1 : 0];
-			fifo_write_en_n 				= 1'b1;
-
-			state_n = S_IDLE;
-		end
-	endcase
 end
 
 assign index_o		= index;

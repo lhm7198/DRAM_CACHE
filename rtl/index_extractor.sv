38214a6b31b40dba5955e01e767d016944d2be57
lhm7198 <lhm7198@naver.com>
2022-09-06 18:32:36 +0900

    FSM

--- rtl/index_extractor.sv
+++ rtl/index_extractor.sv
@@ -1,74 +1,124 @@
 module INDEX_EXTRACTOR # (
-	parameter ADDR_WIDTH = 32,
+	parameter ADDR_WIDTH = 64,
 	parameter DATA_WIDTH = 32,
-	parameter ID_WIDTH = 32,
+	parameter ID_WIDTH = 16,
 	parameter INDEX_BIT_SIZE = 4
 ) (
-	input	wire				clk,
-	input	wire				rst_n,
+	input	wire					clk,
+	input	wire					rst_n,
 
 	// AMBA AXI interface (AR channel)
 	input	wire 	[ID_WIDTH-1 : 0] 		arid_i,
-	input	wire 	[ADDR_WIDTH-1 : 0] 	araddr_i,
-	input	wire				arvalid_i,
+	input	wire 	[ADDR_WIDTH-1 : 0] 		araddr_i,
+	input	wire					arvalid_i,
+	output  	wire				arready_o,
 
 	// AMBA AXI interface (AW channel)
 	input 	wire 	[ID_WIDTH-1 : 0] 		awid_i,
-	input	wire 	[ADDR_WIDTH-1 : 0] 	awaddr_i,
-	input 	wire 				awvalid_i,
-
-	output 	wire 				ready_o,
+	input	wire 	[ADDR_WIDTH-1 : 0] 		awaddr_i,
+	input 	wire 					awvalid_i,
+	output	wire					awready_o,
 
 	// Index extractor -> Memory
-	output 	wire 	[INDEX_BIT_SIZE-1 : 0]   	slave_i,
+	output 	wire 	[INDEX_BIT_SIZE-1 : 0]   	index_o,
 
 	// Index extractor -> FIFO
-	input 	wire				fifo_Afull,
-	output 	wire				fifo_write_enable,
-	output 	wire 	[127 : 0] 			fifo_i 		// 1 + 32 + 32 bit
+	input 	wire					fifo_afull_i,
+	output 	wire					fifo_write_en_o,
+	output 	wire 	[127 : 0] 			fifo_data_o 	// 1 + 32 + 32 bit
 );
 
-reg				ready;
-reg 	[INDEX_BIT_SIZE-1 : 0] 	index;
-reg	[ADDR_WIDTH-1 : 0] 	tag;
-reg	[127 : 0]		fifo_in; 				// 1 + 32 + 32 bit
-reg				write_en;
+localparam 			S_IDLE	= 2'd0,
+				S_RREQ 	= 2'd1,
+				S_WREQ	= 2'd2,
+				S_FIFO	= 2'd3;
 
-always @(posedge clk) begin
+reg	[1:0]			state,		state_n;
+
+reg 	[INDEX_BIT_SIZE-1 : 0] 	index,		index_n;
+reg	[127 : 0]		fifo_data,	fifo_data_n;	// 1 + 32 + 32 bit
+reg				fifo_write_en,	fifo_write_en_n;
+reg				arbiter,	arbiter_n;
+
+reg				arready,
+				awready;
+
+always_ff @(posedge clk)
 	if (!rst_n) begin
-		ready <= 0;
-		write_en <= 0;
+		state		<= S_IDLE;
+		
+		index		<= 4'd0;
+		fifo_data	<= 128'd0;
+		fifo_write_en	<= 0;
+		arbiter		<= 0;
 	end 
 	else begin
-		if(arvalid_i && !ready && !fifo_Afull) begin
-			ready <= 1;
-			index <= araddr_i[INDEX_BIT_SIZE-1 : 0];
-			fifo_in[0:0] <= 0; //read
-			fifo_in[ID_WIDTH:1] <= arid_i;
-			fifo_in[ADDR_WIDTH+ID_WIDTH : ID_WIDTH+1] <= araddr_i[ADDR_WIDTH-1 : 0];
-			write_en <= 1;
-		end
-		else if(awvalid_i && !ready && !fifo_Afull) begin
-			ready <= 1;
-			index <= awaddr_i[INDEX_BIT_SIZE-1 : 0];
-			fifo_in[0:0] <= 1; //write
-			fifo_in[ID_WIDTH:1] <= awid_i;
-			fifo_in[ADDR_WIDTH+ID_WIDTH : ID_WIDTH+1] <= awaddr_i[ADDR_WIDTH-1 : 0];
-			write_en <= 1;
-		end
-		else begin
-			ready <= 0;
-			index <= 0;
-			fifo_in[127:0] <= 0;
-			write_en <= 0;
-		end
+		state		<= state_n;
+
+		index		<= index_n;
+		fifo_data	<= fifo_data_n;
+		fifo_write_en	<= fifo_write_en_n;
+		arbiter		<= arbiter_n;
 	end
+
+always_comb
+begin
+	state_n		= state;
+
+	index_n		= index;
+	fifo_data_n	= fifo_data;
+	fifo_write_en_n	= fifo_write_en;
+	arbiter_n	= arbiter;
+
+	arready		= 1'b1;
+	awready		= 1'b1;
+
+	case (state)
+		S_IDLE: begin
+			if(arvalid_i & !arbiter) begin
+				state_n					= S_RREQ;
+				arbiter_n 				= 1'b1;
+			end
+			else if(awvalid_i & arbiter) begin
+				state_n					= S_WREQ;
+				arbiter_n 				= 1'b0;
+			end
+		end
+		S_RREQ: begin
+			arready						= 1'b0;
+
+			index_n 					= araddr_i[INDEX_BIT_SIZE-1 : 0];
+			fifo_data_n[0:0]				= 1'b0; 				//read
+			if(fifo_afull_i == 0) begin
+				state_n					= S_FIFO;
+			end
+		end
+		S_WREQ: begin
+			awready						= 1'b0;
+
+			index_n 					= araddr_i[INDEX_BIT_SIZE-1 : 0];
+			fifo_data_n[0:0]				= 1'b1; 				//write
+			if(fifo_afull_i == 0) begin
+				state_n					= S_FIFO;
+			end
+		end
+		S_FIFO: begin
+			arready						= 1'b0;
+			awready						= 1'b0;
+
+			fifo_data_n[ID_WIDTH:1]				= arid_i;
+			fifo_data_n[ADDR_WIDTH+ID_WIDTH : ID_WIDTH+1]	= araddr_i[ADDR_WIDTH-1 : 0];
+			fifo_write_en_n 				= 1'b1;
+
+			state_n = S_IDLE;
+		end
+	endcase
 end
 
-assign slave_i = index;
-assign fifo_i = fifo_in;
-assign fifo_write_enable = write_en;
-assign ready_o = ready;
+assign index_o		= index;
+assign fifo_data_o 	= fifo_data;
+assign fifo_write_en_o  = fifo_write_en;
+assign arready_o 	= arready;
+assign awready_o 	= awready;
 
 endmodule
-	
--- sim/tb/INDEX_EXTRACTOR_tb.sv
+++ sim/tb/INDEX_EXTRACTOR_tb.sv
@@ -8,18 +8,18 @@ reg rst_n;
 reg [31 : 0]	arid_i;
 reg [31 : 0]	araddr_i;
 reg		arvalid_i;
+wire		arready_o;
 
 reg [31 : 0]	awid_i;
 reg [31 : 0]	awaddr_i;
 reg		awvalid_i;
+wire		awready_o;
 
-wire		ready_o;
+wire [3 : 0]	index_o;
 
-wire [3 : 0]	slave_i;
-
-reg 		fifo_Afull;
-wire 		fifo_write_enable;
-wire [127 : 0]	fifo_i;
+reg 		fifo_afull_i;
+wire 		fifo_write_en_o;
+wire [127 : 0]	fifo_data_o;
 
 localparam CLOCK_PERIOD = 1000;
 always #(CLOCK_PERIOD/2) clk = ~clk;
@@ -37,7 +37,7 @@ begin
 	awaddr_i = 0;
 	awvalid_i = 0;
 
-	fifo_Afull = 0;
+	fifo_afull_i = 0;
 	
 	#(CLOCK_PERIOD);
 	rst_n = 1'b0;
@@ -50,14 +50,15 @@ begin
 
 	for(i=0 ; i<5 ; i++) begin
 		#(CLOCK_PERIOD);
-		$display("ready_o = %d, slave_i = %d, fifo_write_enable = %d, fifo_i = %d\n", ready_o, slave_i, fifo_write_enable, fifo_i);	
+		//$display("ready_o = %d, slave_i = %d, fifo_write_enable = %d, fifo_i = %d\n", ready_o, slave_i, fifo_write_enable, fifo_i);	
 	
 		if($urandom % 2 == 0) begin
 			arid_i = i;
 			arvalid_i = 1;
 			araddr_i = $urandom % 256;
 			$display("araddr = %d ",araddr_i);
-		end else
+		end 
+		else begin
 			awid_i = i;
 			awvalid_i = 1;
 			awaddr_i = $urandom % 256;
@@ -65,11 +66,11 @@ begin
 		end
 		#(CLOCK_PERIOD);
 
-		$display("ready_o = %d, slave_i = %d, fifo_write_enable = %d, fifo_i = %d\n", ready_o, slave_i, fifo_write_enable, fifo_i);	
+		//$display("ready_o = %d, slave_i = %d, fifo_write_enable = %d, fifo_i = %d\n", ready_o, slave_i, fifo_write_enable, fifo_i);	
 	end
 	$finish;
 end
 
-INDEX_EXTRACTOR index_extractor(.clk(clk), .rst_n(rst_n), .arid_i(arid_i), .araddr_i(araddr_i), .arvalid_i(arvalid_i), .awid_i(awid_i), .awaddr_i(awaddr_i), .awvalid_i(awvalid_i), .ready_o(ready_o), .slave_i(slave_i), .fifo_Afull(fifo_Afull), .fifo_write_enable(fifo_write_enable), .fifo_i(fifo_i));
+INDEX_EXTRACTOR index_extractor(.clk(clk), .rst_n(rst_n), .arid_i(arid_i), .araddr_i(araddr_i), .arvalid_i(arvalid_i), .arready_o(arready_o), .awid_i(awid_i), .awaddr_i(awaddr_i), .awvalid_i(awvalid_i), .awready_o(awready_o), .index_o(index_o), .fifo_afull_i(fifo_afull_i), .fifo_write_en_o(fifo_write_en_o), .fifo_data_o(fifo_data_o));
 
 endmodule

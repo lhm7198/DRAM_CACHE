aff17639de6ec5e73f77613c184429a123227844
lhm7198 <lhm7198@naver.com>
2022-09-13 19:44:43 +0900

    0913

--- rtl/index_extractor.sv
+++ rtl/index_extractor.sv
@@ -1,11 +1,11 @@
-`include "./AXI_TYPEDEF.svh"
+//`include "../rtl/AXI_TYPEDEF.svh"
 
 module INDEX_EXTRACTOR
 #(
-	parameter ADDR_WIDTH	= 'AXI_ADDR_WIDTH,
-	parameter DATA_WIDTH	= 'AXI_DATA_WIDTH,
-	parameter ID_WIDTH	= 'AXI_ID_WIDTH,
-	parameter INDEX_WIDTH	= 'INDEX_WIDTH
+	parameter ADDR_WIDTH	= 64, //`AXI_ADDR_WIDTH,
+	parameter DATA_WIDTH	= 32, //`AXI_DATA_WIDTH,
+	parameter ID_WIDTH	= 16, //`AXI_ID_WIDTH,
+	parameter INDEX_WIDTH	= 4 //`INDEX_WIDTH
 )
 (
 	input	wire					clk,
@@ -46,67 +46,78 @@ reg				arbiter,	arbiter_n;
 reg				arready,
 				awready;
 
-always @(posedge clk) begin
+always_ff @(posedge clk)
 	if (!rst_n) begin
 		state		<= S_IDLE;
 		
 		index		<= 4'd0;
-		fifo_data	<= 128'd0;
-		fifo_write_en	<= 0;
-		arbiter		<= 0;
-
-		state_n		<= S_IDLE;
-		
-		index_n		<= 4'd0;
-		fifo_data_n	<= 128'd0;
-		fifo_write_en_n	<= 0;
-		arbiter_n	<= 0;
-
-	end 
+		fifo_data	<= 81'd0;
+		fifo_write_en	<= 1'b0;
+		arbiter		<= 1'b0;
+	end
 	else begin
-		case (state)
-			S_IDLE: begin
-				if(fifo_afull_i) begin
-					state_n					= state;
-					arbiter_n				= arbiter;
-				end
-				else if(arvalid_i && (!awvalid_i || !arbiter)) begin
-					state_n					= S_RREQ;
-					arbiter_n 				= 1'b1;
-				end
-				else if(awvalid_i && (!arvalid_i || arbiter)) begin
-					state_n					= S_WREQ;
-					arbiter_n 				= 1'b0;
-				end
-			end
-			S_RREQ: begin
-				arready						= 1'b0;
-				index_n 					= araddr_i[INDEX_WIDTH-1 : 0];
-				fifo_data_n[80:80]				= 1'b0; 				//read
-				fifo_data_n[79:64]				= arid_i;
-				fifo_data_n[63:0]				= araddr_i;
-				state_n						= S_IDLE;
-			end
-			S_WREQ: begin
-				awready						= 1'b0;
-				index_n 					= awaddr_i[INDEX_WIDTH-1 : 0];
-				fifo_data_n[80:80]				= 1'b1; 				//write
-				fifo_data_n[79:64]				= awid_i;
-				fifo_data_n[63:0]				= awaddr_i;
-				state_n						= S_IDLE;
-			end
-		endcase
-
 		state		<= state_n;
+		
+		index		<= index_n;	
+		fifo_data	<= fifo_data_n;
+		fifo_write_en	<= fifo_write_en_n;
+		arbiter		<= arbiter_n;
+	end
 
+always_comb begin
+	state_n		= state;
+
+	index_n		= index;
+	fifo_data_n	= fifo_data;
+	fifo_write_en_n = fifo_write_en;
+        arbiter_n	= arbiter;
+
+	arready		= 1'b1;
+	awready		= 1'b1;
+
+	case (state)
+		S_IDLE: begin
+			if(fifo_afull_i) begin
+				state_n					= state;
+				arbiter_n				= arbiter;
+			end
+			else if(arvalid_i && (!awvalid_i || !arbiter)) begin
+				state_n					= S_RREQ;
+				arbiter_n 				= 1'b1;
+			end
+			else if(awvalid_i && (!arvalid_i || arbiter)) begin
+				state_n					= S_WREQ;
+				arbiter_n 				= 1'b0;
+			end
+		end
+		S_RREQ: begin
+			arready						= 1'b0;
+			index_n 					= araddr_i[INDEX_WIDTH-1 : 0];
+			fifo_data_n[80:80]				= 1'b0; 				//read
+			fifo_data_n[79:64]				= arid_i;
+			fifo_data_n[63:0]				= araddr_i;
+			state_n						= S_IDLE;
+		end
+		S_WREQ: begin
+			awready						= 1'b0;
+			index_n 					= awaddr_i[INDEX_WIDTH-1 : 0];
+			fifo_data_n[80:80]				= 1'b1; 				//write
+			fifo_data_n[79:64]				= awid_i;
+			fifo_data_n[63:0]				= awaddr_i;
+			state_n						= S_IDLE;
+		end
+	endcase
+		/*
+		state		<= state_n;
 		index		<= index_n;
 		fifo_data	<= fifo_data_n;
 		fifo_write_en	<= fifo_write_en_n;
 		arbiter		<= arbiter_n;
+	
 		
 		arready		= !fifo_afull_i;
 		awready		= !fifo_afull_i;
-	end
+		*/
 end
 
 assign index_o		= index;

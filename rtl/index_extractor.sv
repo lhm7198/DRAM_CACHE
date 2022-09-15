0aca38b25dccc64b996bf88a06dfbbd25b04c696
lhm7198 <lhm7198@naver.com>
2022-09-15 10:43:56 +0900

    0915

--- rtl/index_extractor.sv
+++ rtl/index_extractor.sv
@@ -29,29 +29,29 @@ module INDEX_EXTRACTOR
 	// Index extractor -> FIFO
 	input 	wire					fifo_afull_i,
 	output 	wire					fifo_write_en_o,
-	output 	wire 	[80 : 0] 			fifo_data_o 	// 1 + 64 + 16 bit
+	output 	wire 	[ADDR_WIDTH + ID_WIDTH : 0] 	fifo_data_o 	// 1 + 64 + 16 bit
 );
 
 localparam 			S_IDLE	= 2'd0,
 				S_RREQ 	= 2'd1,
 				S_WREQ	= 2'd2;
 
-reg	[1:0]			state,		state_n;
+reg	[1:0]					state,		state_n;
 
-reg 	[INDEX_WIDTH-1 : 0] 	index,		index_n;
-reg	[80 : 0]		fifo_data,	fifo_data_n;	// 1 + 64 + 16 bit
-reg				fifo_write_en,	fifo_write_en_n;
-reg				arbiter,	arbiter_n;
+reg 	[INDEX_WIDTH-1 : 0] 			index,		index_n;
+reg	[ADDR_WIDTH + ID_WIDTH : 0]		fifo_data,	fifo_data_n;	// 1 + 64 + 16 bit
+reg						fifo_write_en,	fifo_write_en_n;
+reg						arbiter,	arbiter_n;
 
-reg				arready,
-				awready;
+reg						arready,
+						awready;
 
 always_ff @(posedge clk)
 	if (!rst_n) begin
 		state		<= S_IDLE;
 		
 		index		<= 4'd0;
-		fifo_data	<= 81'd0;
+		fifo_data	<= 0;
 		fifo_write_en	<= 1'b0;
 		arbiter		<= 1'b0;
 	end
@@ -91,33 +91,22 @@ always_comb begin
 			end
 		end
 		S_RREQ: begin
-			arready						= 1'b0;
-			index_n 					= araddr_i[INDEX_WIDTH-1 : 0];
-			fifo_data_n[80:80]				= 1'b0; 				//read
-			fifo_data_n[79:64]				= arid_i;
-			fifo_data_n[63:0]				= araddr_i;
-			state_n						= S_IDLE;
+			arready								= 1'b0;
+			index_n 							= araddr_i[INDEX_WIDTH-1 : 0];
+			fifo_data_n[ADDR_WIDTH + ID_WIDTH : ADDR_WIDTH + ID_WIDTH]	= 1'b0; 	//read
+			fifo_data_n[ADDR_WIDTH + ID_WIDTH - 1 : ADDR_WIDTH]		= arid_i;
+			fifo_data_n[ADDR_WIDTH - 1 : 0]					= araddr_i;
+			state_n								= S_IDLE;
 		end
 		S_WREQ: begin
-			awready						= 1'b0;
-			index_n 					= awaddr_i[INDEX_WIDTH-1 : 0];
-			fifo_data_n[80:80]				= 1'b1; 				//write
-			fifo_data_n[79:64]				= awid_i;
-			fifo_data_n[63:0]				= awaddr_i;
-			state_n						= S_IDLE;
+			awready								= 1'b0;
+			index_n 							= awaddr_i[INDEX_WIDTH-1 : 0];
+			fifo_data_n[ADDR_WIDTH + ID_WIDTH : ADDR_WIDTH + ID_WIDTH]	= 1'b1; 	//write
+			fifo_data_n[ADDR_WIDTH + ID_WIDTH - 1 : ADDR_WIDTH]		= awid_i;
+			fifo_data_n[ADDR_WIDTH -1 : 0]					= awaddr_i;
+			state_n								= S_IDLE;
 		end
 	endcase
-		/*
-		state		<= state_n;
-		index		<= index_n;
-		fifo_data	<= fifo_data_n;
-		fifo_write_en	<= fifo_write_en_n;
-		arbiter		<= arbiter_n;
-	
-		
-		arready		= !fifo_afull_i;
-		awready		= !fifo_afull_i;
-		*/
 end
 
 assign index_o		= index;

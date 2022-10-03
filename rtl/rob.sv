eccf267098d9fa53dfa98fcdd6146f978b80a634
leejh1593 <leejh1593@google.com>
2022-10-03 20:42:57 +0900

    rob

--- rtl/rob.sv
+++ rtl/rob.sv
@@ -1,181 +1,163 @@
-module ROB # ( 
-	parameter DATA_BIT_SIZE = 8,
-	parameter ROB_SIZE = 8
-)(
-	input clk,
-	input reset,
-	
-//hit queue
-	output full_hit,
-	input write_en_hit,
-	input [7:0] write_tID_hit,
-	input [DATA_BIT_SIZE-1:0] write_data_hit,
-	output empty_hit,
-	
-//miss queue
-	output full_miss,
-	input write_en_miss,
-	input [7:0] write_tID_miss,
-	input [DATA_BIT_SIZE-1:0] write_data_miss,
-	output empty_miss,
+`include "TYPEDEF.svh"
 
-	output [DATA_BIT_SIZE-1:0] read_data
+module ROB # (
+	parameter DATA_WIDTH 	= `FIFO_DATA_WIDTH, // 8
+	parameter FIFO_SIZE 	= `FIFO_SIZE, // 8
+	parameter ID_WIDTH  	= `AXI_ID_WIDTH,
+	parameter MAX_TID	= 'MAX_TID
+)(
+	input	wire					clk,
+	input 	wire					rst_n,
+
+
+	output	wire					valid_o,
+	input	wire					ready_i,
+	output	wire	[ID_WIDTH - 1 : 0]		rid_o,
+	output	wire	[DATA_WIDTH - 1 : 0]		rdata_o,
+
+	output	wire					full_hit_o,
+	input	wire					write_en_hit_i,
+	input	wire	[DATA_WIDTH - 1 : 0]		rdata_hit_i,	
+
+	output	wire					full_miss_o,
+	input	wire					write_en_miss_i,
+	input	wire	[DATA_WIDTH - 1 : 0]		rdata_miss_i,	
 
 );
 
-reg	[7:0] tID;
-reg	[7:0] tID_nxt;
+localparam 		S_IDLE		= 3'd0,
+			S_VALID		= 3'd1,
+			S_HIT		= 3'd2,
+			S_MISS		= 3'd3;
 
-reg     [DATA_BIT_SIZE-1:0] mem_hit[ROB_SIZE-1:0];
-reg	[7:0] tID_hit[ROB_SIZE-1:0];
-reg     [7:0] head_hit;
-reg     [7:0] head_hit_nxt;
-reg     head_phase_hit; 
-reg     head_phase_hit_nxt; 
+wire					full_hit;
+wire					write_en_hit;
+wire	[DATA_WIDTH - 1 : 0]		write_data_hit;
 
-reg     [7:0] tail_hit;
-reg     [7:0] tail_hit_nxt;
-reg     tail_phase_hit; 
-reg     tail_phase_hit_nxt; 
+wire					empty_hit;
+wire					read_en_hit;
+wire	[DATA_WIDTH - 1 : 0]		read_data_hit;
 
 
-reg     [DATA_BIT_SIZE-1:0] mem_miss[ROB_SIZE-1:0];
-reg	[7:0] tID_miss[ROB_SIZE-1:0];
-reg     [7:0] head_miss;
-reg     [7:0] head_miss_nxt;
-reg     head_phase_miss; 
-reg     head_phase_miss_nxt; 
+wire					full_miss;
+wire					write_en_miss;
+wire	[DATA_WIDTH - 1 : 0]		write_data_miss;
 
-reg     [7:0] tail_miss;
-reg     [7:0] tail_miss_nxt;
-reg     tail_phase_miss; 
-reg     tail_phase_miss_nxt; 
+wire					empty_miss;
+wire					read_en_miss;
+wire	[DATA_WIDTH - 1 : 0]		read_data_miss;
 
-reg	[DATA_BIT_SIZE-1:0] read_data_nxt;
+reg	[2 : 0]				state, state_n;
 
+reg	[ID_WIDTH - 1 : 0]		tID;
+reg	[ID_WIDTH - 1 : 0]		tID_hit, tID_miss;
+reg	[:]				rdata, rdata_n;
 
-always @(*) begin
-	if (tID == (ROBO_SIZE-1)) begin 
-		tID_nxt = 0;
-	end else begin 
-		tID_nxt = tID + 1;        
-	end
-end
+reg					valid, valid_n;
+reg	[ID_WIDTH - 1 : 0]		rid, rdi_n;
+
+reg					flag;
 
 
-//for hit fifo
-always @(*) begin
-	if (head_hit == (ROBO_SIZE-1)) begin 
-		head_hit_nxt = 0;
-		head_phase_hit_nxt = ~head_phase_hit;
-	end else begin 
-		head_hit_nxt = head_hit + 1;        
-		haed_phase_hit_nxt = head_phase_hit;
-	end
-end
-
-always @(*) begin
-	if (tail_hit == (ROB_SIZE-1)) begin 
-		tail_hit_nxt = 0;
-		tail_phase_hit_nxt = ~tail_phase_hit;
-	end else begin 
-		tail_hit_nxt = tail_hit + 1;        
-		tail_phase_hit_nxt = tail_phase_hit;
-	end
-end
-
-int i;
-always @(posedge clk) begin
-	if (reset) begin 
-		head_hit <= 0;
-		head_phase_hit <= 0;
-		tail_hit <= 0;
-		tail_phase_hit <= 0;
-		for (i=0; i < ROB_SIZE; i++) begin
-			mem_hit[i] <= 0;
-			tID_hit[i] <= 0;
-		end
-	end else begin 
-		if (!full_hit && write_en_hit) begin
-			mem_hit[head_hit] <= write_data_hit;
-		 	tID_hit[head_hit] <= write_tID_hit;	
-			head_hit <= head_hit_nxt;
-			head_phase_hit <= head_phase_hit_nxt;
-		end
-	end
-end
-
-
-//for miss fifo
-always @(*) begin
-	if (head_miss == (ROBO_SIZE-1)) begin 
-		head_miss_nxt = 0;
-		head_phase_miss_nxt = ~head_phase_miss;
-	end else begin 
-		head_miss_nxt = head_miss + 1;        
-		haed_phase_miss_nxt = head_phase_miss;
-	end
-end
-
-always @(*) begin
-	if (tail_miss == (ROB_SIZE-1)) begin 
-		tail_miss_nxt = 0;
-		tail_phase_miss_nxt = ~tail_phase_miss;
-	end else begin 
-		tail_miss_nxt = tail_miss + 1;        
-		tail_phase_miss_nxt = tail_phase_miss;
-	end
-end
-
-int j;
-always @(posedge clk) begin
-	if (reset) begin 
-		head_miss <= 0;
-		head_phase_miss <= 0;
-		tail_miss <= 0;
-		tail_phase_miss <= 0;
-		for (j=0; j < ROB_SIZE; j++) begin
-			mem_miss[j] <= 0;
-			tID_miss[j] <= 0;
-		end
-	end else begin 
-		if (!full_miss && write_en_miss) begin
-			mem_miss[head_miss] <= write_data_miss;
-		 	tID_miss[head_miss] <= write_tID_miss;	
-			head_miss <= head_miss_nxt;
-			head_phase_miss <= head_phase_miss_nxt;
-		end
-	end
-end
-
-
-always @(posedge clk) begin
-	if( tID == tID_hit[tail_hit] && !empty_hit) begin
-		read_data_nxt <= mem_hit[tail_hit];
-		tail_hit <= tail_hit_nxt;
-		tail_phase_hit <= tail_phase_hit_nxt;
-		tID <= tID_nxt;
-	end
-	else if( tID == tID_miss[tail_miss] && !empty_miss) begin
-		read_data_nxt <= mem_miss[tail_miss];
-		tail_miss <= tail_miss_nxt;
-		tail_phase_miss <= tail_phase_miss_nxt;
-		tID <= tID_nxt;
+always_ff @(posedge clk) begin
+	if(!rst_n) begin
+		state		<= S_IDLE;
+		tID		<= 0;
+		tID_hit		<= 0;
+		tID_miss	<= 0;
+		valid		<= 0;
+		rid		<= 0;
+		flag		<= 0;
+		rdata		<= 0;
 	end
 	else begin
-		read_data <= read_data;
-		tail_miss <= tail_miss;
-		tail_phase_miss <= tail_phase_miss;
-		tID <= tID;
+		state		<= state_n;
+		tID		<= tID_n;
+		tID_hit		<= read_data_hit[:];
+		tID_miss	<= read_data_miss[:];
+		valid		<= valid_n;
+		rid		<= rid_n;
+		flag		<= flag_n;
+		rdata		<= rdata_n;
 	end
 end
 
-assign full_hit = (head_phase_hit != tail_phase_hit) && (head_hit == tail_hit);
-assign full_miss = (head_phase_miss != tail_phase_miss) && (head_miss == tail_miss);
+always_comb begin
+	state_n		= state;
+	tID_n		= tID;
+	valid_n		= valid;
+	rid_n		= rid;
+	flag_n		= flag;
 
-assign empty_hit = (head_phase_hit == tail_phase_hit) && (head_hit == tail_hit);
-assign empty_miss = (head_phase_miss == tail_phase_miss) && (head_miss == tail_miss);
+	case (state)
+		S_IDLE: begin
+			if((!empty_hit && tID == tID_hit) || (!empty_miss && tID == tID_miss)) begin
+				if(tID == MAX_TID - 1)
+					tID_n		= 0;
+				else
+					tID_n		= tID + 1;
 
-assign read_data = read_data_nxt; 
+				if(tID == tID_hit) begin
+					flag_n		= 0;
+					rdata_n		= read_data_hit;
+				end
+				else begin
+					flag_n		= 1;
+					rdata_n		= read_data_miss;
+				end
+				state_n			= S_VALID;
+				valid_n			= 1;
+		end
+		S_VAL: begin
+			if(ready_i)    //??
+				valid_n			= 0;
+				state_n			= S_IDLE;
+				if(flag == 0) begin
+					read_en_hit		= 1;
+				end
+				else begin
+					read_en_miss		= 1;
+				end
+		end
+	endcase
+end
 
 
+FIFO	hit_fifo
+(
+	.clk		(clk),
+	.rst_n		(rst_n),
+
+	.full_o		(full_hit),
+	.write_en_i	(write_en_hit_i),
+	.write_data_i	(write_data_hit_i),
+
+	.empty_o	(empty_hit),
+	.read_en_i	(read_en_hit),
+	.read_data_o	(read_data_hit)
+);
+
+
+FIFO	miss_fifo
+(
+	.clk		(clk),
+	.rst_n		(rst_n),
+
+	.full_o		(full_miss),
+	.write_en_i	(write_en_miss_i),
+	.write_data_i	(write_data_miss_i),
+
+	.empty_o	(empty_miss),
+	.read_en_i	(read_en_miss),
+	.read_data_o	(read_data_miss)
+);
+
+assign	valid_o			= valid;
+assign	rid_o			= rid;
+assign	rdata_o			= rdata;
+
+assign	full_hit_o		= full_hit;
+assign	full_miss_o		= full_miss;
+
+
+endmodule

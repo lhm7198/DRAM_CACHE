6eadd7ba0efa03ee1e92ebdd65d0ea9de684cd1a
leejh1593 <leejh1593@google.com>
2022-10-06 19:08:05 +0900

    rob

--- rtl/rob.sv
+++ rtl/rob.sv
@@ -4,7 +4,7 @@ module ROB # (
 	parameter DATA_WIDTH 	= `FIFO_DATA_WIDTH, // 8
 	parameter FIFO_SIZE 	= `FIFO_SIZE, // 8
 	parameter ID_WIDTH  	= `AXI_ID_WIDTH,
-	parameter MAX_TID	= 'MAX_TID
+	parameter TID_MAX	= `TID_MAX
 )(
 	input	wire					clk,
 	input 	wire					rst_n,
@@ -25,33 +25,37 @@ module ROB # (
 
 );
 
-localparam 		S_IDLE		= 3'd0,
-			S_VALID		= 3'd1,
-			S_HIT		= 3'd2,
-			S_MISS		= 3'd3;
+localparam 		S_IDLE		= 1'd0,
+			S_VALID		= 1'd1,
 
+// tag compare - rob
 wire					full_hit;
 wire					write_en_hit;
 wire	[DATA_WIDTH - 1 : 0]		write_data_hit;
 
 wire					empty_hit;
-wire					read_en_hit;
+wire					read_en_hit = en_hit;
 wire	[DATA_WIDTH - 1 : 0]		read_data_hit;
 
-
+// cxl controller - rob
 wire					full_miss;
 wire					write_en_miss;
 wire	[DATA_WIDTH - 1 : 0]		write_data_miss;
 
 wire					empty_miss;
-wire					read_en_miss;
+wire					read_en_miss = en_miss;
 wire	[DATA_WIDTH - 1 : 0]		read_data_miss;
 
+// registers
 reg	[2 : 0]				state, state_n;
 
 reg	[ID_WIDTH - 1 : 0]		tID;
-reg	[ID_WIDTH - 1 : 0]		tID_hit, tID_miss;
+reg	[ID_WIDTH - 1 : 0]		tID_hit = read_data_hit[:];
+reg	[ID_WIDTH - 1 : 0]		tID_miss = read_data_miss[:];
 reg	[:]				rdata, rdata_n;
+
+reg					en_hit, en_hit_n;
+reg					en_miss, en_miss_n;
 
 reg					valid, valid_n;
 reg	[ID_WIDTH - 1 : 0]		rid, rdi_n;
@@ -63,22 +67,22 @@ always_ff @(posedge clk) begin
 	if(!rst_n) begin
 		state		<= S_IDLE;
 		tID		<= 0;
-		tID_hit		<= 0;
-		tID_miss	<= 0;
 		valid		<= 0;
 		rid		<= 0;
 		flag		<= 0;
 		rdata		<= 0;
+		en_hit		<= 0;
+		en_miss		<= 0;
 	end
 	else begin
 		state		<= state_n;
 		tID		<= tID_n;
-		tID_hit		<= read_data_hit[:];
-		tID_miss	<= read_data_miss[:];
 		valid		<= valid_n;
 		rid		<= rid_n;
 		flag		<= flag_n;
 		rdata		<= rdata_n;
+		en_hit		<= en_hit_n;
+		en_miss		<= en_hit_n;
 	end
 end
 
@@ -91,8 +95,10 @@ always_comb begin
 
 	case (state)
 		S_IDLE: begin
+			en_hit_n		= 0;
+			en_missn		= 0;
 			if((!empty_hit && tID == tID_hit) || (!empty_miss && tID == tID_miss)) begin
-				if(tID == MAX_TID - 1)
+				if(tID == TID_MAX - 1)
 					tID_n		= 0;
 				else
 					tID_n		= tID + 1;
@@ -113,10 +119,10 @@ always_comb begin
 				valid_n			= 0;
 				state_n			= S_IDLE;
 				if(flag == 0) begin
-					read_en_hit		= 1;
+					en_hit_n		= 1;
 				end
 				else begin
-					read_en_miss		= 1;
+					en_miss_n		= 1;
 				end
 		end
 	endcase

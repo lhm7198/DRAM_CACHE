6308d45dca54e8c9a09babc476a3707a5f2edf46
leejh1593 <leejh1593@google.com>
2022-10-14 21:41:14 +0900

    10.14

--- rtl/index_extractor.sv
+++ rtl/index_extractor.sv
@@ -41,11 +41,8 @@ module INDEX_EXTRACTOR
 	output 	wire 	[ADDR_WIDTH + TID_WIDTH : 0] 	tag_fifo_data_o 	// 1 + 64 + 16 bit
 );
 
-localparam 			S_IDLE	= 3'd0,
-				S_RRE 	= 3'd1,
-				S_RREQ	= 3'd2,
-				S_WRE	= 3'd3,
-				S_WREQ	= 3'd4;
+localparam 			S_IDLE	= 1'd0,
+				S_REQ 	= 1'd1;
 
 reg	[2 : 0]					state,		state_n;
 
@@ -55,8 +52,7 @@ reg	[ADDR_WIDTH + TID_WIDTH : 0]		tag_fifo_data,	tag_fifo_data_n; // 1 + 64 + 16
 reg						tag_fifo_wren,	tag_fifo_wren_n;
 reg						arbiter,	arbiter_n;
 
-reg						arready,	arready_n,
-						awready,	awready_n,
+reg						arready,	awready,
 						arvalid,	arvalid_n;
 
 always_ff @(posedge clk)
@@ -82,8 +78,6 @@ always_ff @(posedge clk)
 		tag_fifo_wren	<= tag_fifo_wren_n;
 		arbiter		<= arbiter_n;
 		
-		arready		<= arready_n;
-		awready		<= awready_n;
 		arvalid		<= arvalid_n;
 	end
 
@@ -96,13 +90,10 @@ always_comb begin
 	tag_fifo_wren_n = tag_fifo_wren;
         arbiter_n	= arbiter;
 	
-	arready_n	= arready;
-	awready_n	= awready;
 	arvalid_n	= arvalid;
 
 	case (state)
 		S_IDLE: begin
-			$display("S_IDLE\n");
 			if(tag_fifo_afull_i) begin
 				state_n					= state;
 			end
@@ -117,13 +108,14 @@ always_comb begin
 
 				tid_n									= tid + 1;
 
-				arready_n				= 1'b0;
-				awready_n				= 1'b0;
+				arready					= 1'b0;
+				awready					= 1'b0;
 				arvalid_n				= 1'b1;
 
 				arbiter_n 				= 1'b1;
+				tag_fifo_wren_n				= 1'b1;
 				
-				state_n					= S_RRE;
+				state_n					= S_REQ;
 			end
 			else if(awvalid_i & (!arvalid_i | arbiter)) begin
 				index_n[OFFSET_WIDTH - 1 : 0]						= 0;
@@ -134,51 +126,27 @@ always_comb begin
 				tag_fifo_data_n[ADDR_WIDTH + TID_WIDTH - 1 : ADDR_WIDTH]		= 0;
 				tag_fifo_data_n[ADDR_WIDTH + TID_WIDTH : ADDR_WIDTH + TID_WIDTH]	= 1'b1;
 
-				arready_n				= 1'b0;
-				awready_n				= 1'b0;
+				arready					= 1'b0;
+				awready					= 1'b0;
 				arvalid_n				= 1'b1;
 				
 				arbiter_n 				= 1'b0;
+				tag_fifo_wren_n				= 1'b1;
 
-				state_n					= S_WRE;
+				state_n					= S_REQ;
 			end
 			else begin
 				state_n					= state;
 			end
 		end
-		S_RRE: begin
-			$display("S_RRE\n");
+		S_REQ: begin
+			tag_fifo_wren_n				= 1'b0;
+			arready					= 1'b1;
+			awready					= 1'b1;
 			if(arready_i) begin
-				tag_fifo_wren_n						= 1'b1;
 				arvalid_n						= 1'b0;
-				
-				state_n 						= S_RREQ;
+				state_n 						= S_IDLE;
 			end
-		end
-		S_RREQ: begin
-			$display("S_RREQ\n");
-			arready_n							= 1'b1;
-			awready_n							= 1'b1;
-			tag_fifo_wren_n							= 1'b0;
-
-			state_n								= S_IDLE;
-		end
-		S_WRE: begin
-			$display("S_WRE\n");
-			if(arready_i) begin
-				tag_fifo_wren_n						= 1'b1;
-				arvalid_n						= 1'b0;
-		
-				state_n 						= S_WREQ;
-			end
-		end
-		S_WREQ: begin
-			$display("S_WREQ\n");
-			arready_n							= 1'b1;
-			awready_n							= 1'b1;
-			tag_fifo_wren_n							= 1'b0;
-		
-			state_n								= S_IDLE;
 		end
 	endcase
 end
